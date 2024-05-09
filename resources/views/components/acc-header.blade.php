0748d351aa35cd5a3f2dcab721cb94b46216c882
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2024-05-09 07:42:05 +0700

    Update acc-header.blade.php

--- resources/views/components/acc-header.blade.php
+++ resources/views/components/acc-header.blade.php
@@ -1,15 +1,38 @@
-@props(['isCreate' => true, 'isSearch' => true, 'originRoute' => ''])
+@props([
+    'isCreate' => true,
+    'isSearch' => true,
+    'isPaginate' => true,
+    'originRoute' => '',
+    'createFunction' => 'create'
+])
 
 <div>
     <div class="row mb-3">
-        @if($isSearch)
-            <div class="col-md-{{ $isCreate ? '6' : '12' }}">
-                <x-acc-search />
+        <div class="col-md-12">
+            <div class="float-start">
+                @if($isCreate)
+                    <x-acc-create-btn
+                        :route="$originRoute"
+                        :createFunction="$createFunction"
+                    />
+                @endif
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        @if($isPaginate)
+            <div class="col-md-{{ $isSearch ? '6' : '12' }}">
+                <select class="form-control" wire:model.live="paginate">
+                    <option value="10">10 Records Per Page</option>
+                    <option value="25">25 Records Per Page</option>
+                    <option value="50">50 Records Per Page</option>
+                    <option value="100">100 Records Per Page</option>
+                </select>
             </div>
         @endif
-        @if($isCreate)
-            <div class="col-md-{{ $isSearch ? '6' : '12' }}">
-                <x-acc-create-btn :route="$originRoute ?? ''" />
+        @if($isSearch)
+            <div class="col-md-{{ $isPaginate ? '6' : '12' }}">
+                <x-acc-search />
             </div>
         @endif
         {{ $slot->isEmpty() ? '' : $slot }}
