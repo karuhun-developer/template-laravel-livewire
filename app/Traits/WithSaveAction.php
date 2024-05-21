1dc2b6d8e2f04f4ca7fd5f0184d423a1561438b4
bayurifkialghifari <bayurifkialgh@gmail.com>
2024-05-21 11:14:22 +0700

    delete unused code

--- app/Traits/WithSaveAction.php
+++ app/Traits/WithSaveAction.php
@@ -6,7 +6,6 @@ use App\Enums\Alert;
 
 trait WithSaveAction {
     public function save() {
-        // try {
         $this->form->save();
 
         // Image iteration to prevent duplicate image
@@ -14,8 +13,5 @@ trait WithSaveAction {
 
         $this->dispatch('closeModal', modal: 'acc-modal');
         $this->dispatch('alert', type: Alert::success, message: $this->isUpdate ? 'Data Updated' : 'Data Created');
-        // } catch (\Exception $exception) {
-        //     $this->dispatch('alert', type: Alert::error, message: $exception->getMessage());
-        // }
     }
 }
