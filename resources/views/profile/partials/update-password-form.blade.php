a464877e4d20f0ea2da472a8228984eefa174d6f
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2024-06-07 07:12:01 +0700

    Update update-password-form.blade.php

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -9,10 +9,6 @@
         </p>
     </header>
 
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
-        @csrf
-    </form>
-
     <form method="post" action="{{ route('password.update') }}" class="mb-5">
         @csrf
         @method('put')
