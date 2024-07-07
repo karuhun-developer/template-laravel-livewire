6ffc1fa91f64cd93f89c824496a51cc8f51b3dee
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2024-07-07 10:24:24 +0700

    Update IsApiRequest.php

--- app/Traits/IsApiRequest.php
+++ app/Traits/IsApiRequest.php
@@ -5,7 +5,7 @@ namespace App\Traits;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Contracts\Validation\Validator;
 
-trait isApiRequest {
+trait IsApiRequest {
     public function failedValidation(Validator $validator)
     {
         throw new HttpResponseException(response()->json([
