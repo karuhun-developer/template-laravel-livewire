920117621830382d2bd0053e42276afa388b9c2a
bayurifkialghifari <bayurifkialgh@gmail.com>
2024-07-16 08:19:11 +0700

    add search debounce

--- resources/views/components/acc-search.blade.php
+++ resources/views/components/acc-search.blade.php
@@ -3,6 +3,6 @@
         <span class="input-group-text" id="search-icon">
             <i class="align-middle" data-feather="search"></i>
         </span>
-        <input wire:model.live="search" type="text" class="form-control" aria-label="Search" aria-describedby="search-icon" placeholder="Search...">
+        <input wire:model.live.debounce.250="search" type="text" class="form-control" aria-label="Search" aria-describedby="search-icon" placeholder="Search...">
     </div>
 </div>
