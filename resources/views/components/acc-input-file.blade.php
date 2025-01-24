9b2225303b60499e8afe4467b6cb030e16735dab
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-01-24 14:34:55 +0700

    Update acc-input-file.blade.php

--- resources/views/components/acc-input-file.blade.php
+++ resources/views/components/acc-input-file.blade.php
@@ -4,5 +4,6 @@
     'imageIttr' => 1,
 ])
 <x-acc-upload-progress>
-    <input type="file" wire:model="{{ $model }}" class="{{ $class }}" id="{{ $imageIttr }}">
+    <input type="file" wire:model="{{ $model }}" class="{{ $class }}" id="{{ $imageIttr }}" {{ $attributes ?? '' }}>
 </x-acc-upload-progress>
+<x-acc-input-error for="{{ $model }}" />
