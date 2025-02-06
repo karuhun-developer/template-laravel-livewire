3b507912cbb9c0a3d72f47f17df67dabd15768c6
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-02-06 17:07:39 +0700

    Update acc-trix-editor-upload.blade.php

--- resources/views/components/acc-trix-editor-upload.blade.php
+++ resources/views/components/acc-trix-editor-upload.blade.php
@@ -7,66 +7,52 @@
 
 <div wire:ignore>
     <input id="{{ $id }}" type="hidden" name="{{ $model_name }}" value="{{ $model }}">
-    <trix-editor input="{{ $id }}" class="trix-content"></trix-editor>
+    <trix-editor wire:ignore input="{{ $id }}" class="trix-content"
+        x-data="{
+            onHoldAttachments: [],
+            uploadTrixImage(attachment) {
+                this.onHoldAttachments.push(attachment.file)
+
+                // Upload
+                @this.upload(
+                    '{{ $file_model_name }}',
+                    attachment.file,
+                    function(uploadedURL) {
+                        // We need to create a custom event.
+                        // This event will create a pause in thread execution until we get the Response URL from the Trix Component completeUpload
+                        const trixUploadCompletedEvent = `trix-upload-completed:${btoa(uploadedURL)}`
+                        const trixUploadCompletedListener = function(event) {
+                            attachment.setAttributes(event.detail)
+                            window.removeEventListener(trixUploadCompletedEvent, trixUploadCompletedListener)
+                        }
+
+                        window.addEventListener(trixUploadCompletedEvent, trixUploadCompletedListener)
+
+                        // call the Trix Component @completeUpload below
+                        @this.call('completeUpload', uploadedURL, trixUploadCompletedEvent)
+                    },
+                    function() {
+                        // Error callback
+                    },
+                    function(event) {
+                        // Progress callback
+                        attachment.setUploadProgress(event.detail.progress)
+                    },
+                    // complete the upload and get the actual file URL
+                )
+            }
+        }"
+        x-on:trix-blur="
+            @this.set('{{ $model_name }}', document.getElementById('{{ $id }}').getAttribute('value'))
+        "
+        x-on:trix-file-accept="if (!['image/png', 'image/jpeg', 'image/jpg'].includes($event.file.type)) {
+            // file type not allowed, prevent default upload
+            return $event.preventDefault()
+        }"
+        x-on:trix-attachment-add="uploadTrixImage($event.attachment)"
+        x-on:trix-attachment-remove="@this.removeUpload('{{ $file_model_name }}', ev.attachment.attachment.previewURL)"
+    ></trix-editor>
 
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
     <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
 </div>
-
-<script>
-    const trixEditor = document.getElementById('{{ $id }}')
-    const mimeTypes = ['image/png', 'image/jpeg', 'image/jpg']
-    const onHoldAttachments = []
-
-    addEventListener('trix-blur', ev => {
-        @this.set('{{ $model_name }}', trixEditor.getAttribute('value'))
-    })
-
-    addEventListener('trix-file-accept', ev => {
-        if (!mimeTypes.includes(ev.file.type)) {
-            console.log('File type not allowed')
-            // file type not allowed, prevent default upload
-            return ev.preventDefault()
-        }
-    })
-
-    addEventListener('trix-attachment-add', ev => {
-        uploadTrixImage(ev.attachment)
-    })
-
-    addEventListener('trix-attachment-remove', ev => {
-        @this.removeUpload('{{ $file_model_name }}', ev.attachment.attachment.previewURL)
-    })
-
-    function uploadTrixImage(attachment) {
-        onHoldAttachments.push(attachment.file)
-
-        // Upload
-        @this.upload(
-            '{{ $file_model_name }}',
-            attachment.file,
-            function(uploadedURL) {
-                // We need to create a custom event.
-                // This event will create a pause in thread execution until we get the Response URL from the Trix Component completeUpload
-                const trixUploadCompletedEvent = `trix-upload-completed:${btoa(uploadedURL)}`
-                const trixUploadCompletedListener = function(event) {
-                    attachment.setAttributes(event.detail[0])
-                    window.removeEventListener(trixUploadCompletedEvent, trixUploadCompletedListener)
-                }
-
-                window.addEventListener(trixUploadCompletedEvent, trixUploadCompletedListener)
-
-                // call the Trix Component @completeUpload below
-                @this.call('completeUpload', uploadedURL, trixUploadCompletedEvent)
-            },
-            function() {
-                // Error callback
-            },
-            function(event) {
-                // Progress callback
-                attachment.setUploadProgress(event.detail.progress)
-            },
-            // complete the upload and get the actual file URL
-        )
-    }
-</script>
