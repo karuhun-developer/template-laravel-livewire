1bb3f8e5c03d5f46bd8c5e50955b062b67efef81
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-04-14 01:49:26 +0700

    Update acc-form.blade.php

--- resources/views/components/acc-form.blade.php
+++ resources/views/components/acc-form.blade.php
@@ -1,4 +1,8 @@
-@props(['submit', 'reset' => 'resetAll'])
+@props([
+    'submit',
+    'withReset' => true,
+    'reset' => 'resetAll'
+])
 
 <div {{ $attributes->merge(['class' => 'container mt-2']) }}>
     <form wire:submit="{{ $submit }}">
@@ -16,16 +20,18 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="float-end">
-                        <button
-                            class="btn btn-warning"
-                            wire:loading.attr="disabled"
-                            wire:click="{{ $reset }}"
-                            type="button">
-                            <i class="fa fa-redo"></i>
-                            <span class="ms-2">
-                                Reset
-                            </span>
-                        </button>
+                        @if($withReset)
+                            <button
+                                class="btn btn-warning"
+                                wire:loading.attr="disabled"
+                                wire:click="{{ $reset }}"
+                                type="button">
+                                <i class="fa fa-redo"></i>
+                                <span class="ms-2">
+                                    Reset
+                                </span>
+                            </button>
+                        @endif
                         <button
                             class="btn btn-success"
                             wire:loading.attr="disabled"
