175362aeb55ffc2d594d2696184f0614ad9b2075
bayurifkialghifari <bayurifkialgh@gmail.com>
2025-06-07 15:50:54 +0700

    update

--- app/Livewire/Cms/Management/Setting/TermOfService.php
+++ app/Livewire/Cms/Management/Setting/TermOfService.php
@@ -8,15 +8,22 @@ use BaseComponent;
 class TermOfService extends BaseComponent
 {
     public FormTermOfService $form;
-    public $title = 'Term Of Service';
+    public $title = 'Terms of Service';
     public $isUpdate = true;
+    public $trix = '';
 
     public function mount() {
         $this->form->getData();
+        $this->trix = $this->form->term_of_service;
     }
 
     public function render()
     {
         return view('livewire.cms.management.setting.term-of-service')->title($this->title);
+    }
+
+    public function customSave() {
+        $this->form->term_of_service = $this->trix;
+        $this->save();
     }
 }
