7c98d2997bef129e9e7d34c296dd7a1457a92e5c
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-08-06 11:52:10 +0700

    Update WithGenerateReference.php

--- app/Traits/WithGenerateReference.php
+++ app/Traits/WithGenerateReference.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 trait WithGenerateReference {
 
-    public function generateReference(Model|Builder $model, string $prefix = 'GGWP', $field = 'ref_number') : array {
-        $reference = $prefix . '-';
+    public function generateReference(Model|Builder $model, string $prefix = '', string $suffix = '', $field = 'ref_number') : array {
+        $reference = $prefix;
         $latest = $model->latest()->first();
         $latest = $latest ? $latest->{$field} : 0;
 
-        $reference = $reference . str_pad($latest + 1, 5, "0", STR_PAD_LEFT);
+        $reference = $reference . str_pad($latest + 1, 5, "0", STR_PAD_LEFT) . $suffix;
 
         return [
             'code' => $reference,
