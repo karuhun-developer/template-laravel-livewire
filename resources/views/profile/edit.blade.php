ca9681c5ae1dfd0ce7106d152b216fb9ebbec91b
bayurifkialghifari <bayurifkialgh@gmail.com>
2025-08-23 09:01:45 +0700

    add back button profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -11,8 +11,9 @@
                 default => null,
             };
         @endphp
+        <x-acc-back url="{{ url()->previous()  }}" />
         <x-acc-custom-alert session="status" :message="$message" />
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 mt-3">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
