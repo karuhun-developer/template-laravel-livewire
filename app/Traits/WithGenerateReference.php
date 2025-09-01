a28289751cfa78c8c0b765001f2ea14404cfef45
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-09-01 15:31:21 +0700

    Update WithGenerateReference.php

--- app/Traits/WithGenerateReference.php
+++ app/Traits/WithGenerateReference.php
@@ -9,7 +9,7 @@ trait WithGenerateReference {
 
     public function generateReference(Model|Builder $model, string $prefix = '', string $suffix = '', $field = 'ref_number') : array {
         $reference = $prefix;
-        $latest = $model->latest()->first();
+        $latest = $model->orderBy($field, 'desc')->first();
         $latest = $latest ? $latest->{$field} : 0;
 
         $reference = $reference . str_pad($latest + 1, 5, "0", STR_PAD_LEFT) . $suffix;
