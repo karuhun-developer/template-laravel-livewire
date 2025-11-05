912a3837c4e24c278889650211573e45288cb527
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-11-05 11:13:51 +0700

    Refactor API routes to use versioning

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'V1 API',
-    ]);
-})->name('welcome');
+Route::group(['prefix' => 'v1', 'as' => 'api.v1.'], function () {
+    // Welcome Route
+    Route::get('/', function () {
+        return response()->json([
+            'message' => 'V1 API',
+        ]);
+    })->name('welcome');
 
-// Authentication Routes
-require 'v1/auth.php';
+    // Authentication Routes
+    require 'v1/auth.php';
+});
