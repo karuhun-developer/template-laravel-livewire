df5c2653859755e912a4dc4adb302d8914018715
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-11-17 15:18:01 +0700

    Update app-logo.blade.php

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,5 +2,7 @@
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
+    <span class="mb-0.5 truncate leading-tight font-semibold">
+        {{ config('app.name', 'Laravel') }}
+    </span>
 </div>
