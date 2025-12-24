fe369abd340ca4c50550883ae48e3ff2f74714b4
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-12-24 19:25:04 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Traits\WithReturnResponse;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 abstract class Controller
 {
-    use WithReturnResponse, AuthorizesRequests;
+    use WithReturnResponse;
+
+    protected function authorize(string $ability, mixed $arguments = null): void
+    {
+        if (! auth()->user()?->can($ability, $arguments)) {
+            throw new \Illuminate\Auth\Access\AuthorizationException('This action is unauthorized.', 403);
+        }
+    }
 }
