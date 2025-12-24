63d01c7d055222d958cd160253dd9b88dac4347b
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2025-12-24 19:07:35 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Traits\WithReturnResponse;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 abstract class Controller
 {
-    use WithReturnResponse;
+    use WithReturnResponse, AuthorizesRequests;
 }
