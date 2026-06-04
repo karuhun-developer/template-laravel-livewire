7d5eb88db44a92ec45c1fa301c29172a67688a11
bayurifkialghifari <bayurifkialgh@gmail.com>
2026-06-04 14:20:42 +0700

    fix auth api

--- app/Http/Controllers/Api/V1/Auth/PasswordResetController.php
+++ app/Http/Controllers/Api/V1/Auth/PasswordResetController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Api\V1\Auth;
 
-use App\Actions\Api\V1\Auth\ResendAuthenticatedAction;
+use App\Actions\Api\V1\Auth\ResetPasswordAction;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\V1\Management\Auth\ResetPasswordRequest;
+use App\Http\Requests\Api\V1\Auth\ResetPasswordRequest;
 
 class PasswordResetController extends Controller
 {
     /**
      * Handle the incoming password reset request.
      */
-    public function store(ResetPasswordRequest $request, ResendAuthenticatedAction $action)
+    public function store(ResetPasswordRequest $request, ResetPasswordAction $action)
     {
         if (! $action->handle($request->validated())) {
             return $this->responseWithError('Unable to resend verification email.', 422);
